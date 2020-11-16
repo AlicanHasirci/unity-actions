3f04c5759c71688326a70b5ae9cacb28334d3bbd
Alican HASIRCI <alishex@gmail.com>
2020-11-17 00:38:42 +0300

    dummy tests

---
+++ Assets/Editor/Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Editor {
+    public class Test
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            Assert.True(true);
+        }
+    }
+}
